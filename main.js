b533f722ce12d7e3092b90b2b2aedc8e87a65ad7
SaishGawade01 <86872880+SaishGawade01@users.noreply.github.com>
2022-05-12 13:47:49 +0530

    Changed variable names

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-var menuImage = document.getElementById("mainMenuImg");
+var menuImg = document.getElementById("mainMenuImg");
 var menuDiv = document.getElementById("menuDiv");
-var bodyX = document.getElementById("mainBody");
+var mainBodyTag = document.getElementById("mainBody");
 var screenCover = document.getElementById("screenCover");
 
-menuImage.addEventListener("click", toggleMenu);
+menuImg.addEventListener("click", toggleMenu);
 
 screenCover.addEventListener("click", toggleMenu);
 
-var xmlgd = 0;
+var menuStatusCount = 0;
+
 function toggleMenu()
 {
-  if(xmlgd === 0)
+  if (menuStatusCount === 0)
   {
-   menuDiv.style.left="0";
-   xmlgd=1;
-   bodyX.style.overflow="hidden";
-   screenCover.style.display="inline-block";
+    menuDiv.style.left = "0";
+    menuStatusCount = 1;
+    mainBodyTag.style.overflow  "hidden";
+    screenCover.style.display = "inline-block";
   }
-  else{
-    menuDiv.style.left ="-300px";
-    xmlgd=0;
-    bodyX.style.overflow="auto";
-    screenCover.style.display="none";
+  else {
+    menuDiv.style.left = "-390px";
+    menuStatusCount = 0;
+    mainBodyTag.style.overflow = "initial";
+    screenCover.style.display = "none";
   }
-  
+
 }
